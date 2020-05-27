b198cc5e3f7eff48b3c5f9d47019731375976b05
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-05-27 15:40:38 -0700

    Disable other services in proxy

--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const {createProxyMiddleware} = require('http-proxy-middleware');
-const port = 80;
-const path = require('path');
+const port = 8000;
 
-const apiProxy = createProxyMiddleware({target: 'http://ec2-3-132-5-204.us-east-2.compute.amazonaws.com:3001'});
-const reviewProxy = createProxyMiddleware({target: 'http://18.212.184.37:3004'});
-const relatedProxy = createProxyMiddleware({target: 'http://54.89.234.183:3003'});
-const itemsProxy = createProxyMiddleware({target: 'http://34.201.53.74:3002'});
 app.use(express.static('public'))
 
-app.use('/api/items/:id',apiProxy);
-app.use('/api/allreviews/',reviewProxy);
+// const apiProxy = createProxyMiddleware({target: 'http://ec2-3-132-5-204.us-east-2.compute.amazonaws.com:3001'});
+// const reviewProxy = createProxyMiddleware({target: 'http://18.212.184.37:3004'});
+const relatedProxy = createProxyMiddleware({
+  target: 'http://localhost:3003/',
+  changeOrigin: true,
+});
+// const itemsProxy = createProxyMiddleware({target: 'http://34.201.53.74:3002'});
+
+// app.use('/api/items/:id',apiProxy);
+// app.use('/api/allreviews/',reviewProxy);
 app.use('/api/related_products/:id',relatedProxy);
-app.use('/items/:id',itemsProxy);
+// app.use('/items/:id',itemsProxy);
 
+// app.get('/:id', (req, res) => {
+//   res.send('public');
+// })
 
+app.use(bodyParser.json()); // THIS NEEDS TO BE HERE AT THE END (WHY AGAIN?)
 
-
-
-
-app.get('/:id', (req, res) => {
-  res.send('public');
-})
 app.listen(port,() => {
-  console.log('listening..');
+  console.log('Proxy server listening on port: ', port);
 });
